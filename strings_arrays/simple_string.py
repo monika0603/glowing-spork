5c8ae13a36151d86e9da212841ef4197ae3b1391
Monika Sharma <monika.pu@gmail.com>
2022-07-06 14:36:29 -0400

    String concatenation

---
+++ strings_arrays/simple_string.py
@@ -0,0 +1,11 @@
+""" 
+Write a function greet that takes in a string argument, s, and returns the string "hey s". 
+No tricks here. Run the tests to check your work.
+"""
+
+def greet(s):
+    return ("hey "+s)
+
+# Driver code 
+s = "alvin"
+print(greet(s))
