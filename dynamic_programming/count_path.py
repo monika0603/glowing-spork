5de5abd0b69cca1460bbde5187f23190da840ca0
Monika Sharma <monika.pu@gmail.com>
2022-07-11 15:56:49 -0400

    Number of ways to travel from one end of the grid to another

--- dynamic_programming/count_path.py
+++ dynamic_programming/count_path.py
@@ -32,25 +32,52 @@ where we want to be. So we should return a count of 1.
 
 def count_paths(grid):
     r,c = 0,0
-    return _count_path(grid, r, c) 
+    return _count_path(grid, r, c, {}) 
 
-def _count_path(grid, r, c):
+def _count_path(grid, r, c, memo):
+    # Storing the row and column position as a tuple
+    pos = (r,c)
+    # If the tuple already exists in the memo, then simply return it
+    if pos in memo:
+        return memo[pos]
     # Base case to not count if we are outside of any cell boundaries
-    if r == len(grid) or c == len(grid[0]):
+    if r == len(grid) or c == len(grid[0]) or grid[r][c] == 'X':
         return 0 
     # If I am already at the bottom right corner then return 1 
     if r == len(grid)-1 and c == len(grid[0])-1:
         return 1 
 
-    down_count = _count_path(grid, r+1, c)
-    right_count = _count_path(grid, r, c+1)
-    return down_count + right_count
+    down_count = _count_path(grid, r+1, c, memo)
+    right_count = _count_path(grid, r, c+1, memo)
+    # Otherwise store the information in the memo
+    memo[pos] = down_count + right_count
+    return memo[pos]
 
 # Driver code
 # Test case 01 
 grid = [ 
     ["0", "0"],
     ["0", "0"]
+]
+print(count_paths(grid))
+
+# Test case 02
+grid = [
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
 ]
 print(count_paths(grid))
     
