d5df091b80a60a2741dc173f15e553201a7b7f66
Monika Sharma <monika.pu@gmail.com>
2022-07-14 17:27:13 -0400

    minimum number of words needed to build the string by concatenating words of the list

--- dynamic_programming/quickest_concat.py
+++ dynamic_programming/quickest_concat.py
@@ -4,6 +4,9 @@ The function should return the minimum number of words needed to build the strin
 words of the list.
 
 You may use words of the list as many times as needed.
+
+Example: s = caution
+words = ['ca', 'ut', 'ion', 'caut']
 """
 # Brute force solution, using recursion without memoization
 def quickest_concat(s, words):
@@ -13,18 +16,25 @@ def _quickest_concat(s, words):
     # Base case: is we reach an empty string, then return True
     if s == '':
         return 0 
+    
+    min_words = float("inf")
     # Loop over the list of words provided to chop of the suffix from the string if it exists
     for word in words:
         if s.startswith(word):
             suffix = s[len(word):]
             # Recur on the suffix now, thereby shrinking the length of the string and increment the count by 1
-            min_words = 1 + _quickest_concat(suffix, words)
+            attempt = 1 + _quickest_concat(suffix, words)
+            min_words = min(attempt, min_words)
 
     return min_words 
 
 # Optimized solution, with memoization
 def quickest_concat_memo(s, words):
-    return _quickest_concat_memo(s, words, {})
+    result = _quickest_concat_memo(s, words, {})
+    if result == float("inf"):
+        return -1 
+    else:
+        return result
 
 def _quickest_concat_memo(s, words, memo):
     if s in memo:
@@ -32,11 +42,12 @@ def _quickest_concat_memo(s, words, memo):
 
     if s == '':
         return 0 
-
+    min_words = float("inf")
     for word in words:
         if s.startswith(word):
             suffix = s[len(word):]
-            min_words = 1 + _quickest_concat_memo(suffix, words, memo)
+            attempt = 1 + _quickest_concat_memo(suffix, words, memo)
+            min_words = min(attempt, min_words)
             memo[s] = min_words 
 
     return memo[s]
@@ -66,3 +77,8 @@ print(quickest_concat(s, words))
 s = 'uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu'
 words = ['u', 'uu', 'uuu', 'uuuu', 'uuuuu']
 print(quickest_concat_memo(s, words))
+
+# Test case #6
+s = 'simchacindy'
+words = ['sim', 'simcha', 'acindy', 'ch']
+print(quickest_concat(s, words))
