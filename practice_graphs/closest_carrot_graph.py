e94faf4735d3bb72cfe142eb5e65a7c15c8e44e0
Monika Sharma <monika.pu@gmail.com>
2022-07-18 17:53:15 -0400

    length of the shortest path from the starting position to a carrot in mxn grid

--- practice_graphs/closest_carrot_graph.py
+++ practice_graphs/closest_carrot_graph.py
@@ -36,7 +36,9 @@ def closest_carrot(grid, r, c):
                    queue.append((neighbor_row, neighbor_col, distance+1))
 
     return -1
-    
+
+# Driver code
+# Test case 01    
 grid = [
   ['O', 'O', 'O', 'O', 'O'],
   ['O', 'X', 'O', 'O', 'O'],
@@ -47,3 +49,65 @@ grid = [
 ]
 
 print(closest_carrot(grid, 1, 2)) # --> 4
+
+# Test 02 
+grid = [
+  ['O', 'O', 'O', 'O', 'O'],
+  ['O', 'X', 'O', 'O', 'O'],
+  ['O', 'X', 'X', 'O', 'O'],
+  ['O', 'X', 'C', 'O', 'O'],
+  ['O', 'X', 'X', 'O', 'O'],
+  ['C', 'O', 'O', 'O', 'O'],
+]
+
+print(closest_carrot(grid, 0, 0)) # -> 5
+
+# Test 03
+grid = [
+  ['O', 'O', 'X', 'X', 'X'],
+  ['O', 'X', 'X', 'X', 'C'],
+  ['O', 'X', 'O', 'X', 'X'],
+  ['O', 'O', 'O', 'O', 'O'],
+  ['O', 'X', 'X', 'X', 'X'],
+  ['O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'C', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O'],
+]
+
+print(closest_carrot(grid, 3, 4)) # -> 9
+
+# Test case 04
+grid = [
+  ['O', 'O', 'X', 'O', 'O'],
+  ['O', 'X', 'X', 'X', 'O'],
+  ['O', 'X', 'C', 'C', 'O'],
+]
+
+print(closest_carrot(grid, 1, 4)) # -> 2
+
+# Test case 05
+grid = [
+  ['O', 'O', 'X', 'O', 'O'],
+  ['O', 'X', 'X', 'X', 'O'],
+  ['O', 'X', 'C', 'C', 'O'],
+]
+
+print(closest_carrot(grid, 2, 0)) # -> -1
+
+# Test case 06
+grid = [
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'X'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'C'],
+]
+
+print(closest_carrot(grid, 0, 0)) # -> -1
