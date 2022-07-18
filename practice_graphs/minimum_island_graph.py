a8a6ad95f610c1fa2718a31f1f46e6e66116e212
Monika Sharma <monika.pu@gmail.com>
2022-07-18 17:06:34 -0400

    Size of smallest island in a mxn grid

---
+++ practice_graphs/minimum_island_graph.py
@@ -0,0 +1,85 @@
+"""" 
+Write a function, minimum_island, that takes in a grid containing Ws and Ls. W represents 
+water and L represents land. The function should return the size of the smallest island. 
+An island is a vertically or horizontally connected region of land.
+
+You may assume that the grid contains at least one island.
+"""
+
+def minimum_island(grid):
+
+    visited = set() 
+    min_island = float("inf")
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            size = explore(grid, r, c, visited)
+            if size > 0:
+                min_island = min(size, min_island) 
+
+    return min_island
+
+def explore(grid, r, c, visited):
+
+    row_inbounds = 0 <= r < len(grid)
+    col_inbounds = 0 <= c < len(grid[0])
+
+    if not row_inbounds or not col_inbounds:
+        return 0 
+
+    if grid[r][c] == 'W':
+        return 0 
+
+    pos = (r,c)
+    if pos in visited:
+        return 0 
+
+    visited.add(pos) 
+
+    size = 1 
+    size += explore(grid, r-1, c, visited)
+    size += explore(grid, r+1, c, visited)
+    size += explore(grid, r, c+1, visited)
+    size += explore(grid, r, c-1, visited)
+
+    return size 
+
+# Driver code
+# Test case 01
+grid = [
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'W', 'W', 'L', 'W'],
+  ['W', 'W', 'L', 'L', 'W'],
+  ['L', 'W', 'W', 'L', 'L'],
+  ['L', 'L', 'W', 'W', 'W'],
+]
+print(minimum_island(grid))
+
+# Test case 02 
+grid = [
+  ['L', 'W', 'W', 'L', 'W'],
+  ['L', 'W', 'W', 'L', 'L'],
+  ['W', 'L', 'W', 'L', 'W'],
+  ['W', 'W', 'W', 'W', 'W'],
+  ['W', 'W', 'L', 'L', 'L'],
+]
+print(minimum_island(grid)) # -> 1
+
+# Test case 03
+grid = [
+  ['L', 'L', 'L'],
+  ['L', 'L', 'L'],
+  ['L', 'L', 'L'],
+]
+
+print(minimum_island(grid)) # -> 9
+
+# Test case 04
+grid = [
+  ['W', 'W'],
+  ['L', 'L'],
+  ['W', 'W'],
+  ['W', 'L']
+]
+
+print(minimum_island(grid)) # -> 1
