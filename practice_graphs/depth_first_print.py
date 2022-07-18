0a5742c07e0561e134d5bf6acf5e32d7105114e5
Monika Sharma <monika.pu@gmail.com>
2022-07-18 11:56:21 -0400

    DFS traversal of a graph

---
+++ practice_graphs/depth_first_print.py
@@ -0,0 +1,28 @@
+""" 
+Depth-first-print of a graph
+"""
+
+graph = { 
+    "a": ["b", "c"],
+    "b": ["d"],
+    "c": ["e"],
+    "d": ["f"],
+    "e": [],
+    "f": []
+}
+
+def depth_first_print(graph, start):
+
+    stack = [start] # Current stack [a]
+
+    while stack:
+        current = stack.pop()
+
+        print(current)
+        # FIFO operation will add b and c to the stack
+        # [b, c]
+        # Since is the end, it gets printed first and so on
+        for neighbor in graph[current]: 
+            stack.append(neighbor)
+
+print(depth_first_print(graph, "a"))
